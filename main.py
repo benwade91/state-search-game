e052792bdd48c5bff8fb78938e2be33d5ea149e7
benwade91 <70988204+benn925@users.noreply.github.com>
2021-08-21 10:54:33 -0700

    added list of missing states for testing

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ screen.bgpic('blank_states_img.gif')
 
 data = pandas.read_csv('50_states.csv')
 
+state_list = data.state.tolist()
+print(state_list)
 
 def add_state(state,x,y):
     state_label = turtle.Turtle()
@@ -20,9 +22,11 @@ def add_state(state,x,y):
 game_on = True
 
 while game_on:
-    state_search = screen.textinput('States', 'enter the name of a state?').lower().capitalize()
-    search = (data[data['state'] == state_search])
-    add_state(search.state.tolist()[0], search.x, search.y)
-
+    state_search = screen.textinput('States', 'enter the name of a state?').lower().title()
+    if state_search in data.values:
+        search = (data[data['state'] == state_search])
+        add_state(search.state.tolist()[0], search.x, search.y)
+        state_list.remove(state_search)
+        print(state_list)
 
 turtle.exitonclick()
